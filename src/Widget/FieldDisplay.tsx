36ff1bd14196213a742181305fc25ab1e4e7033d
Gabriel Fortin <gabriel.14.fortin@gmail.com>
2020-04-25 19:38:07 +0100

    Add some styling to 'FieldDisplay'

--- src/Widget/FieldDisplay.tsx
+++ src/Widget/FieldDisplay.tsx
@@ -2,14 +2,17 @@ import React from 'react';
 
 import { Field } from 'Model';
 
+import './fieldDisplay.css';
+
+
 interface FieldProps {
     field: Field;
 }
 
 export const FieldDisplay: React.FC<FieldProps> = ({field}) =>
-    <>
-        <div>HELLO Field</div>
-        <div>
-            {JSON.stringify(field)}
-        </div>
-    </>
+    <tr className={"field-display"}>
+        <td>{field.title}</td>
+        <td>{field.label}</td>
+        <td>{field.hintText ?? "—"}</td>
+        <td>{field.validationRules}</td>
+    </tr>
--- src/Widget/Page.tsx
+++ src/Widget/Page.tsx
@@ -13,7 +13,12 @@ export const Page: React.FC<PageProps> = ({ page }) =>
     <div style={{textAlign: "left"}}>
         HELLO Page
         {JSON.stringify(page)}
-        <div style={{marginLeft: "4em", marginTop: "1em"}}>
-            {page.content.map(fieldOrGroup => <FieldDisplay field={fieldOrGroup as FieldModel} />)}
-        </div>
+        <table style={{marginLeft: "4em", marginTop: "1em"}}>
+            {page.content.map(fieldOrGroup =>
+                <FieldDisplay
+                    key={fieldOrGroup.id}
+                    field={fieldOrGroup as FieldModel}
+                />
+            )}
+        </table>
     </div>
