178198c0d17995b920974a92eb1b6ced599f5f34
Gabriel Fortin <gabriel.14.fortin@gmail.com>
2020-04-25 15:26:15 +0100

    Add redux store (without reducers yet)

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,22 @@
 import React from 'react';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import logo from './logo.svg';
 import './App.css';
+import State from './Store/State';
 
 function App() {
+  const identityOrDefault = (s: State | undefined, a: any) => s ?? new State();
+  
+  const reducer: (s: State | undefined, a: any) => State = identityOrDefault;
+  const store = createStore(reducer, new State());
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Provider store={store}>
+
+        </Provider>
       </header>
     </div>
   );
