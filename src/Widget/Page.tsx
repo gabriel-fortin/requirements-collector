3cac7e302b7c9e348f77ff992812976d6ba9258e
Fortin <GTSGFOR@gov.im>
2020-05-01 16:56:24 +0100

    Remove unnecessary parts ('FieldDisplay')

--- src/Widget/Page.tsx
+++ src/Widget/Page.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
-import { Box, Text } from '@chakra-ui/core';
+import { Text } from '@chakra-ui/core';
 
-import { Page as PageModel, Field as FieldModel, Field, FieldType as FieldTypeModel } from 'Model';
+import {
+    Page as PageModel,
+    Field as FieldModel,
+    FieldType as FieldTypeModel
+} from 'Model';
 
-import { FieldDisplay } from './FieldDisplay';
 import { FieldEditor } from './FieldEditor';
 import './page.css';
 
@@ -14,36 +17,8 @@ interface PageProps {
 
 export const Page: React.FC<PageProps> = ({ page }) =>
     <article className={"page"}>
-        HELLO Page
-        <br />
-        <Text as="div" fontSize={1} color="grey">
-            {JSON.stringify(page)}
+        <Text as="h1" textAlign="center" marginTop={0}>
+            Circus Service
         </Text>
-        <table>
-            <tbody>
-                <tr>
-                    <LocalHeader text="title" />
-                    <LocalHeader text="hint text" />
-                    <LocalHeader text="req / opt" />
-                    <LocalHeader text="validation" />
-                </tr>
-                {page.content.map(fieldOrGroup =>
-                    <FieldDisplay
-                        key={fieldOrGroup.id}
-                        field={fieldOrGroup as FieldModel}
-                    />
-                )}
-            </tbody>
-        </table>
-        <FieldEditor field={new Field(true, { kind: "text", multiLine: false, maxCharacters: 55 } as FieldTypeModel, "Favourite Toy")} />
+        <FieldEditor field={new FieldModel(true, { kind: "text", multiLine: false, maxCharacters: 55 } as FieldTypeModel, "Favourite Toy")} />
     </article>;
-
-interface HProps { text: string; }
-const LocalHeader: React.FC<HProps> = ({ text }) => 
-    <th>
-        <Box textAlign="center" px={2} mb={2}>
-            <Text as="span" className="header-text" size="xs" px={1}>
-                {text}
-            </Text>
-        </Box>
-    </th>;
